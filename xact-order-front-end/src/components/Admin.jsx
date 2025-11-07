76d2acbe5114580cc1dbbb49b1a0b3c2e5ff4816
BryJime <bry.jimenex@gmail.com>
2025-11-06 17:19:31 -0800

    Updated styling

--- xact-order-front-end/src/components/Admin.jsx
+++ xact-order-front-end/src/components/Admin.jsx
@@ -4,34 +4,39 @@
 function Admin() {
     return (
         <div>
-            <form className="addExam-form">
-                <label htmlFor="examName">Exam Name:</label>
-                <input type="text" id="examName" name="examName" required />
+            <form className="add-exam-form">
 
-                <label htmlFor="examViews">Views:</label>
-                <input type="text" id="examViews" name="examViews" required />
-
-                <label htmlFor="examCPT">CPT Code:</label>
-                <input type="text" id="examCPT" name="examCPT" required />
-
-                <label htmlFor="examDescription">Description:</label>
-                <textarea id="examDescription" name="examDescription" required />
-
-                <label htmlFor="examAlias">Alias:</label>
-                <input type="text" id="examAlias" name="examAlias" required />
-
-                <label htmlFor="examRegion">Region:</label>
-                <input type="text" id="examRegion" name="examRegion" required />
-
-                <label class="toggle-switch">Common Exam:
-                    <input type="checkbox" />
-                        <span class="slider"></span>
+                <label htmlFor="examName">Exam Name:
+                <input className="exam-input" type="text" id="examName" name="examName" required />
                 </label>
 
-                <button type="reset">Clear</button>
-                <button type="submit">Save Exam</button>
+                <label htmlFor="examViews">Views:
+                <input className="exam-input" type="text" id="examViews" name="examViews" required />
+                </label>
+
+                <label htmlFor="examCPT">CPT Code:
+                <input className="exam-input" type="text" id="examCPT" name="examCPT" required />
+                </label>
+
+                <label htmlFor="examDescription">Description:
+                <textarea className="exam-input"id="examDescription" name="examDescription" required />
+                </label>
+
+                <label htmlFor="examAlias">Alias:
+                <input className="exam-input" type="text" id="examAlias" name="examAlias" required />
+                </label>
+
+                <label htmlFor="examRegion">Region:
+                <input className="exam-input" type="text" id="examRegion" name="examRegion" required />
+                </label>
+
+                <label className="common-label">Common Exam: 
+                    <input type="checkbox" />      
+                </label>
 
 
+                <button className="admin-button" type="reset">Clear</button>
+                <button className="admin-button" type="submit">Save Exam</button>
             </form>
 
             <hr className="line"/>
