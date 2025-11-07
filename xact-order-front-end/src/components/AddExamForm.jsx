9aa834bfd296ee0810424a70c08b1dda08bdffd2
BryJime <bry.jimenex@gmail.com>
2025-11-06 21:34:17 -0800

    Created Separate Add Exam component

---
+++ xact-order-front-end/src/components/AddExamForm.jsx
@@ -0,0 +1,42 @@
+
+
+function AddExamForm() {
+
+
+  return <form className="add-exam-form">
+
+                <label htmlFor="examName">Exam Name:
+                    <input className="exam-input" type="text" id="examName" name="examName" required />
+                </label>
+
+                <label htmlFor="examViews">Views:
+                    <input className="exam-input" type="text" id="examViews" name="examViews" required />
+                </label>
+
+                <label htmlFor="examCPT">CPT Code:
+                    <input className="exam-input" type="text" id="examCPT" name="examCPT" required />
+                </label>
+
+                <label htmlFor="examDescription">Description:
+                    <textarea className="exam-input" id="examDescription" name="examDescription" required />
+                </label>
+
+                <label htmlFor="examAlias">Alias:
+                    <input className="exam-input" type="text" id="examAlias" name="examAlias" required />
+                </label>
+
+                <label htmlFor="examRegion">Region:
+                    <input className="exam-input" type="text" id="examRegion" name="examRegion" required />
+                </label>
+
+                <label className="common-label">Common Exam:
+                    <input type="checkbox" />
+                </label>
+
+
+                <button className="admin-button" type="reset">Clear</button>
+                <button className="admin-button" type="submit">Save Exam</button>
+            </form>
+}
+
+export default AddExamForm;
--- xact-order-front-end/src/components/Admin.jsx
+++ xact-order-front-end/src/components/Admin.jsx
@@ -1,7 +1,9 @@
 import ExamsData from "./ExamData/ExamsData";
+import AddExamForm from "./AddExamForm";
 
 
 function Admin() {
+    
 
     let allExams = ExamsData.map(exam => {
 
@@ -13,7 +15,7 @@ function Admin() {
             <td>{exam.description.text}</td>
             <td>{exam.alias.name}</td>
             <td>{exam.region}</td>
-            <td>{exam.commonExam ? "Yes" : "No"}</td>
+            <td>{exam.common ? "Yes" : "No"}</td>
         </tr>)
 
     })
@@ -23,40 +25,7 @@ function Admin() {
 
     return (
         <div>
-            <form className="add-exam-form">
-
-                <label htmlFor="examName">Exam Name:
-                    <input className="exam-input" type="text" id="examName" name="examName" required />
-                </label>
-
-                <label htmlFor="examViews">Views:
-                    <input className="exam-input" type="text" id="examViews" name="examViews" required />
-                </label>
-
-                <label htmlFor="examCPT">CPT Code:
-                    <input className="exam-input" type="text" id="examCPT" name="examCPT" required />
-                </label>
-
-                <label htmlFor="examDescription">Description:
-                    <textarea className="exam-input" id="examDescription" name="examDescription" required />
-                </label>
-
-                <label htmlFor="examAlias">Alias:
-                    <input className="exam-input" type="text" id="examAlias" name="examAlias" required />
-                </label>
-
-                <label htmlFor="examRegion">Region:
-                    <input className="exam-input" type="text" id="examRegion" name="examRegion" required />
-                </label>
-
-                <label className="common-label">Common Exam:
-                    <input type="checkbox" />
-                </label>
-
-
-                <button className="admin-button" type="reset">Clear</button>
-                <button className="admin-button" type="submit">Save Exam</button>
-            </form>
+            <AddExamForm />
 
             <hr className="line" />
 
